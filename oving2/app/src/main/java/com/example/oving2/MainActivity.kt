1aad50cce4d48a9df0a1c40788ebd56b824bfcb4
Cecilie Vu <cecilie_vu@hotmail.com>
2026-09-25 16:37:37 +0200

    la til alle compose komponentene

--- oving2/app/src/main/java/com/example/oving2/MainActivity.kt
+++ oving2/app/src/main/java/com/example/oving2/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.oving2
 
+import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.oving2.ui.theme.Oving2Theme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 data class Recipe(
@@ -33,14 +53,127 @@ val sampleRecipes: List<Recipe> = listOf(
 )
 
 class MainActivity : ComponentActivity() {
-
+    override fun onCreate (savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Oving2Theme {
+                val context = LocalContext.current
+                val message = stringResource(R.string.info_message)
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    innerPadding ->
+                    RecipeScreen(
+                        recipes = sampleRecipes,
+                        onRecipeClick = { recipe ->
+                            Toast.makeText(context, recipe.title,
+                                Toast.LENGTH_SHORT).show()
+                        },
+                        onInfoClick = {
+                            Toast.makeText(context, message,
+                                Toast.LENGTH_SHORT).show()
+                        },
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
+            }
+        }
     }
+}
 
+// hele skjermen med header, oppskriftsliste og knapp nederst
+@Composable
+fun RecipeScreen(
+    recipes: List<Recipe>,
+    onRecipeClick: (Recipe) -> Unit,
+    onInfoClick: () -> Unit,
+    modifier: Modifier = Modifier
+        .background(MaterialTheme.colorScheme.primary)
+) {
+    Column(modifier = modifier.fillMaxSize()) {
+        RecipeHeader()
+        ClickableRecipeList(
+            recipes = recipes,
+            onRecipeClick = onRecipeClick,
+            modifier = Modifier.weight(1f),
+        )
+        Button(
+            onClick = onInfoClick,
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondary,
+            )
+        ) {
+            Text(text = stringResource(R.string.show_info))
+        }
+    }
+}
+
+// viser klikkbar liste med oppskrifter
+@Composable
+fun ClickableRecipeList (
+    recipes: List<Recipe>,
+    onRecipeClick: (Recipe) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier,
+    ) {
+       items(recipes) { recipe ->
+           RecipeListItem(
+               recipe = recipe,
+               modifier = Modifier.clickable{ onRecipeClick(recipe) },
+           )
+       }
+    }
+}
+
+// header med tittel høst og ukens oppskrifter
+@Composable
+fun RecipeHeader(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxWidth().height(160.dp),
+    ) {
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = stringResource(R.string.header_description),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize(),
+        )
+        Text(
+            text = stringResource(R.string.screen_title1),
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(15.dp)
+                .background(MaterialTheme.colorScheme.secondary)
+                .padding(10.dp)
+        )
+        Text(
+            text = stringResource(R.string.screen_title),
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(15.dp)
+                .background(MaterialTheme.colorScheme.tertiary)
+                .padding(10.dp)
+
+        )
+    }
+}
+
+// viser en item med tittel, kategori og tid
 @Composable
 fun RecipeListItem(recipe: Recipe, modifier: Modifier = Modifier) {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = modifier.fillMaxWidth().padding(16.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.tertiary)
+            .padding(15.dp)
+
     ) {
         Column {
             Text(
@@ -53,32 +186,24 @@ fun RecipeListItem(recipe: Recipe, modifier: Modifier = Modifier) {
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.onSurface,
             )
+
         }
         Text(
-            text = recipe.time,
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.primary,
-        )
-    }
-}
-
-@Composable
-fun RecipeList(recipe: List<Recipe>, modifier: Modifier = Modifier) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        contentPadding = PaddingValues(vertical = 8.dp),
-        modifier = modifier
-    ) {
-        items(recipe) { recipe ->
-            RecipeListItem(recipe = recipe)
+                text = recipe.time,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.secondary,
+            )
         }
     }
-}
 
 @Preview(showBackground = true)
 @Composable
-fun RecipeListItemPreview() {
+fun RecipeScreenPreview() {
     Oving2Theme {
-        RecipeList(recipe = sampleRecipes)
+        RecipeScreen(
+            recipes = sampleRecipes,
+            onRecipeClick = {},
+            onInfoClick = {},
+        )
     }
 }
--- oving2/app/src/main/java/com/example/oving2/ui/theme/Type.kt
+++ oving2/app/src/main/java/com/example/oving2/ui/theme/Type.kt
@@ -8,13 +8,17 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Serif,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
